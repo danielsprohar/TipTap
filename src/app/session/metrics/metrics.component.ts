a7728fb22c24847cc01632c1abfa0877f7bc8376
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-24 14:43:45 -0500

    Reset the metrics when the session is reset

--- src/app/session/metrics/metrics.component.ts
+++ src/app/session/metrics/metrics.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { SessionService } from '../services/session.service'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core'
+import { Subject, takeUntil } from 'rxjs'
 import { MetricsService } from '../services/metrics.service'
+import { SessionService } from '../services/session.service'
 
 @Component({
   selector: 'tiptap-metrics',
@@ -11,8 +17,22 @@ import { MetricsService } from '../services/metrics.service'
   styleUrls: ['./metrics.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MetricsComponent {
+export class MetricsComponent implements OnDestroy, OnInit {
+  private readonly destroy$ = new Subject<void>()
   readonly metrics$ = this.metricsService.metrics$
 
-  constructor(private readonly metricsService: MetricsService) {}
+  constructor(
+    private readonly metricsService: MetricsService,
+    private readonly sessionService: SessionService
+  ) {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next()
+  }
+
+  ngOnInit(): void {
+    this.sessionService.reset$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.metricsService.reset())
+  }
 }
