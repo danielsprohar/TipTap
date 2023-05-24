cac5f1916235d651b5fb3d8724373d6e4d094745
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-24 01:06:07 -0500

    ChangedDetectionStrategy

--- src/app/components/snackbar/snackbar.component.ts
+++ src/app/components/snackbar/snackbar.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core'
+import { AsyncPipe, NgIf } from '@angular/common'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { Observable, tap } from 'rxjs'
 import { SnackbarService } from 'src/app/services/snackbar.service'
-import { NgIf, AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-snackbar',
-    template: `
+  selector: 'app-snackbar',
+  template: `
     <ng-container *ngIf="message$ | async as message">
       <div id="snackbar">
         {{ message }}
       </div>
     </ng-container>
   `,
-    styleUrls: ['./snackbar.component.scss'],
-    standalone: true,
-    imports: [NgIf, AsyncPipe],
+  styleUrls: ['./snackbar.component.scss'],
+  standalone: true,
+  imports: [NgIf, AsyncPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SnackbarComponent implements OnInit {
   message$?: Observable<string>
--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,32 +1,29 @@
-import { Component, OnInit } from '@angular/core'
-import { NgFor } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { MatDividerModule } from '@angular/material/divider';
+import { NgFor } from '@angular/common'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { MatDividerModule } from '@angular/material/divider'
+import { RouterLink } from '@angular/router'
 
 const FINGER_NAME_TO_COLOR_MAP = [
   { finger: 'Little (Pinky)', color: 'Yellow-Green', hexColorCode: '#befc75' },
   { finger: 'Ring', color: 'Lavendar Blue', hexColorCode: '#c0dbfc' },
   { finger: 'Middle', color: 'Aqua Blue (Water)', hexColorCode: '#ccffff' },
-  { finger: 'Index (Pointy)', color: 'Yellow (Laser Lemon)', hexColorCode: '#ffff66' },
+  {
+    finger: 'Index (Pointy)',
+    color: 'Yellow (Laser Lemon)',
+    hexColorCode: '#ffff66',
+  },
   { finger: 'Thumb', color: 'Brilliant Lavender', hexColorCode: '#e9befc' },
 ]
 
 @Component({
-    selector: 'app-welcome',
-    templateUrl: './welcome.component.html',
-    styleUrls: ['./welcome.component.scss'],
-    standalone: true,
-    imports: [
-        MatDividerModule,
-        RouterLink,
-        NgFor,
-    ],
+  selector: 'tiptap-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatDividerModule, RouterLink, NgFor],
 })
-export class WelcomeComponent implements OnInit {
-  tableColumns = ['finger', 'color', 'hex']
-  dataSource = FINGER_NAME_TO_COLOR_MAP
-
-  constructor() {}
-
-  ngOnInit(): void {}
+export class WelcomeComponent {
+  readonly tableColumns = ['finger', 'color', 'hex']
+  readonly dataSource = FINGER_NAME_TO_COLOR_MAP
 }
