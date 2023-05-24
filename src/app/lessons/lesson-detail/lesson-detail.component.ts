69151a23c2221b93b630103ae109b5a067fdd25f
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-24 01:11:15 -0500

    LessonDetail: change detection

--- src/app/lessons/lesson-detail/lesson-detail.component.ts
+++ src/app/lessons/lesson-detail/lesson-detail.component.ts
@@ -1,39 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { NgFor, NgIf, TitleCasePipe } from '@angular/common'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
+import { RouterLink } from '@angular/router'
 import { CharacterSpaceBuilder } from '../builders/character-space-builder'
 import { Lesson } from '../models/lesson'
-import { RouterLink } from '@angular/router';
-import { MatLegacyCardModule } from '@angular/material/legacy-card';
-import { NgIf, NgFor, TitleCasePipe } from '@angular/common';
 
 @Component({
-    selector: 'app-lesson-detail',
-    templateUrl: './lesson-detail.component.html',
-    styleUrls: ['./lesson-detail.component.scss'],
-    standalone: true,
-    imports: [
-        NgIf,
-        MatLegacyCardModule,
-        RouterLink,
-        NgFor,
-        TitleCasePipe,
-    ],
+  standalone: true,
+  selector: 'tiptap-lesson-detail',
+  templateUrl: './lesson-detail.component.html',
+  styleUrls: ['./lesson-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgIf, MatCardModule, RouterLink, NgFor, TitleCasePipe],
 })
 export class LessonDetailComponent implements OnInit {
-  @Input() lesson?: Lesson
+  @Input({ required: true }) lesson!: Lesson
   keys?: string[]
   link?: string
 
   constructor() {}
 
   ngOnInit(): void {
-    if (this.lesson) {
-      this.keys = new CharacterSpaceBuilder(this.lesson).build()
-      this.link =
-        this.lesson.hand + (this.lesson.hand === 'both' ? ' Hands' : ' Hand')
+    if (!this.lesson) {
+      throw new Error('Lesson is required')
+    }
 
-      if (this.lesson.finger && this.lesson.finger !== 'all') {
-        this.link += ` - ${this.lesson.finger} Finger`
-      }
+    this.keys = new CharacterSpaceBuilder(this.lesson).build()
+    this.link =
+      this.lesson.hand + (this.lesson.hand === 'both' ? ' Hands' : ' Hand')
+
+    if (this.lesson.finger && this.lesson.finger !== 'all') {
+      this.link += ` - ${this.lesson.finger} Finger`
     }
   }
 }
