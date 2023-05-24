68d42eeda864b3448561ee7b551b0d7ba7edfb9b
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-24 01:05:37 -0500

    Added a missing import to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,29 +4,29 @@ import {
   LayoutModule,
 } from '@angular/cdk/layout'
 import { OverlayContainer } from '@angular/cdk/overlay'
-import { AsyncPipe, DOCUMENT } from '@angular/common'
+import { AsyncPipe } from '@angular/common'
 import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
-  Inject,
   OnDestroy,
   OnInit,
 } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterLink, RouterOutlet } from '@angular/router'
-import { AuthService } from '@auth0/auth0-angular'
 import { Observable, Subject, map, shareReplay, takeUntil } from 'rxjs'
 
 @Component({
-  selector: 'app-root',
+  selector: 'tiptap-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [
+    MatButtonModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
@@ -54,9 +54,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor(
     private readonly observer: BreakpointObserver,
-    private readonly overlay: OverlayContainer,
-    public readonly auth: AuthService,
-    @Inject(DOCUMENT) public document: Document
+    private readonly overlay: OverlayContainer
   ) {}
 
   ngOnDestroy(): void {
@@ -80,9 +78,5 @@ export class AppComponent implements OnInit, OnDestroy {
           this.overlay.getContainerElement().classList.remove('dark-theme')
         }
       })
-  }
-
-  logout() {
-    this.auth.logout({ returnTo: document.location.origin })
   }
 }
