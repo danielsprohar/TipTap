6c00584de3c7ccb5dc79fcfed4334c441be6145b
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-24 13:03:25 -0500

    Removed Auth0 module

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core'
 
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser'
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideRouter } from '@angular/router'
-import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular'
 import { appRoutes } from './app/app-routes'
 import { AppComponent } from './app/app.component'
 import { environment } from './environments/environment'
@@ -20,16 +15,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
-    importProvidersFrom(
-      BrowserModule,
-      AuthModule.forRoot({
-        ...environment.auth,
-        httpInterceptor: {
-          ...environment.httpInterceptor,
-        },
-      })
-    ),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+    importProvidersFrom(BrowserModule),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
   ],
