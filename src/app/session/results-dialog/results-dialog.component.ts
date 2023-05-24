f8079a98c377223cabb0c5dd94388e5ebe9a8847
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-24 11:08:40 -0700

    Added the metrics service to the results dialog

--- src/app/session/results-dialog/results-dialog.component.ts
+++ src/app/session/results-dialog/results-dialog.component.ts
@@ -1,4 +1,4 @@
-import { NgIf, PercentPipe } from '@angular/common'
+import { AsyncPipe, NgIf, PercentPipe } from '@angular/common'
 import {
   ChangeDetectionStrategy,
   Component,
@@ -13,21 +13,32 @@ import {
 } from '@angular/material/dialog'
 import { RouterLink } from '@angular/router'
 import { Metrica } from '../models/metrica'
+import { MetricsService } from '../services/metrics.service'
 import { SessionService } from '../services/session.service'
 
 @Component({
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'tiptap-results-dialog',
   templateUrl: './results-dialog.component.html',
   styleUrls: ['./results-dialog.component.scss'],
-  imports: [MatDialogModule, NgIf, MatButtonModule, RouterLink, PercentPipe],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    MatDialogModule,
+    NgIf,
+    MatButtonModule,
+    RouterLink,
+    PercentPipe,
+    AsyncPipe,
+  ],
 })
 export class ResultsDialogComponent implements OnInit {
+  readonly metrics$ = this.metricsService.metrics$
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Metrica,
     private readonly sessionService: SessionService,
-    private readonly dialogRef: MatDialogRef<ResultsDialogComponent>
+    private readonly dialogRef: MatDialogRef<ResultsDialogComponent>,
+    private readonly metricsService: MetricsService
   ) {}
 
   ngOnInit(): void {}
