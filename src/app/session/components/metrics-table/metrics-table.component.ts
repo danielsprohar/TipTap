446828900cc38a489d423fda5bdb0eea919e5587
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-27 14:27:16 -0700

    Angular Material table

--- src/app/session/components/metrics-table/metrics-table.component.ts
+++ src/app/session/components/metrics-table/metrics-table.component.ts
@@ -1,15 +1,25 @@
 import { CommonModule, DecimalPipe } from '@angular/common'
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { MatTableModule } from '@angular/material/table'
 import { TimeSeriesSample } from '../../models/time-series-sample'
 
 @Component({
   selector: 'tiptap-metrics-table',
   standalone: true,
-  imports: [CommonModule, DecimalPipe],
+  imports: [CommonModule, DecimalPipe, MatTableModule],
   templateUrl: './metrics-table.component.html',
   styleUrls: ['./metrics-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MetricsTableComponent {
   @Input({ required: true }) timeSeries!: TimeSeriesSample[]
+  readonly displayedColumns: string[] = [
+    "deltaSeconds",
+    "cpm",
+    "characterCount",
+    "wordCount",
+    "wpm",
+    "errorCount",
+    "accuracy"
+  ]
 }
