9f16c778bac1304669ab720f555a3353d3948672
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-05-29 11:35:41 -0500

    Made the RWG service a singleton

--- src/app/session/services/random-word-generator.service.ts
+++ src/app/session/services/random-word-generator.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core'
 import { environment } from 'src/environments/environment'
 import { CharacterSpace } from '../../lessons/character-space'
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class RandomWordGeneratorService {
   private readonly defaultWordCount = environment.rwg.defaults.wordCount
   private readonly defaultWordSize = environment.rwg.defaults.wordSize
