40c47f113f6e02a55f6a3552cc80fc0ff9f6e88f
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-11-17 16:34:20 -0600

    Update Prettier configuration, add Prettier as a dependency, and remove unused app component tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
-import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
-import { MatButtonModule } from '@angular/material/button'
-import { MatIconModule } from '@angular/material/icon'
-import { MatListModule } from '@angular/material/list'
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { RouterLink, RouterOutlet } from '@angular/router'
-import { Observable } from 'rxjs'
-import { HandsetService } from './services/handset.service'
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { Observable } from "rxjs";
+import { HandsetService } from "./services/handset.service";
 
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  selector: 'tiptap-app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "tiptap-app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
   imports: [
     CommonModule,
     MatButtonModule,
@@ -27,7 +27,7 @@ import { HandsetService } from './services/handset.service'
   ],
 })
 export class AppComponent {
-  private readonly handsetService = inject(HandsetService)
+  private readonly handsetService = inject(HandsetService);
 
-  readonly isHandset$: Observable<boolean> = this.handsetService.isHandset$
+  readonly isHandset$: Observable<boolean> = this.handsetService.isHandset$;
 }
