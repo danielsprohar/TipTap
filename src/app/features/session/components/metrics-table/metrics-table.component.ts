8274e5189cb18d745aea2c2052de5add31565ae1
Daniel Sprohar <daniel.sprohar@gmail.com>
2025-01-01 14:24:01 -0600

    Refactor metrics table component to use standard HTML table and remove unused styles

--- src/app/features/session/components/metrics-table/metrics-table.component.ts
+++ src/app/features/session/components/metrics-table/metrics-table.component.ts
@@ -1,33 +1,13 @@
-import { CommonModule, DecimalPipe } from "@angular/common";
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnInit,
-} from "@angular/core";
-import { MatTableModule } from "@angular/material/table";
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import { SessionResults } from "../../models/session-results";
 
 @Component({
-    selector: "tiptap-metrics-table",
-    imports: [CommonModule, DecimalPipe, MatTableModule],
-    templateUrl: "./metrics-table.component.html",
-    styleUrls: ["./metrics-table.component.scss"],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "tiptap-metrics-table",
+  imports: [CommonModule],
+  templateUrl: "./metrics-table.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MetricsTableComponent implements OnInit {
-  @Input({ required: true }) sessionResults!: SessionResults;
-  readonly dataSource: SessionResults[] = [];
-  readonly displayedColumns: string[] = [
-    "accuracy",
-    "rawWPM",
-    "netWPM",
-    "totalWords",
-    "totalWordsWithErrors",
-    "totalCharacters",
-  ];
-
-  ngOnInit(): void {
-    this.dataSource.push(this.sessionResults);
-  }
+export class MetricsTableComponent {
+  readonly sessionResults = input.required<SessionResults>();
 }
